c0f77be652af422ecb861fae2193c25f1851a464
Mike Mathew <m2mathew@me.com>
2015-09-11 19:25:47 -0500

    Normal mode is operational, still working on a reset button

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
 // STEP ONE: target elements
@@ -9,8 +10,6 @@ var resetButton = document.getElementById("resetButton");
 
 var doArray = [];
 var newListItem = "";
-
-
 
 // STEP TWO: create function
 
@@ -24,29 +23,29 @@ function render() {
     }
 }
 
-
 // STEP THREE: Add event listener
 
 // clear text in input text area
 doLabel.addEventListener("click", clearText);
 
 // add list item to array when button is clicked and add it to the printed list
-addButton.addEventListener("click", function() {
+addButton.addEventListener("click", function () {
     doArray.push(doLabel.value);
     render();
 });
 
-
-
 // reset ALL list data and start over
-resetButton.addEventListener("click", function() {
+resetButton.addEventListener("click", function () {
     var doArray = [];
     var newListItem = "";
     mainList.innerHTML = "";
 });
 
-
-function clearText(){
-  document.getElementById("doLabel").value = "";
+function clearText() {
+    document.getElementById("doLabel").value = "";
 }
 
+},{}]},{},[1])
+
+
+//# sourceMappingURL=bundle.js.map
